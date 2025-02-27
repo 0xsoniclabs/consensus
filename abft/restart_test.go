5b5159a0a40eac78fb3b42d3b6011a61e5c8341f
Matija Kupresanin <87671707+kmeeth@users.noreply.github.com>
2025-02-27 18:33:24 +0100

    Fixed capitalization oversight from merging.

--- abft/restart_test.go
+++ abft/restart_test.go
@@ -183,7 +183,7 @@ func testRestartAndReset(t *testing.T, weights []pos.Weight, mutateWeights bool,
 				return memorydb.New()
 			}
 
-			restored := NewIndexedLachesis(store, prev.input, &adapters.VectorToDagIndexer{Engine: vecengine.NewIndex(prev.crit, vecengine.LiteConfig(), vecengine.GetEngineCallbacks())}, prev.crit, prev.config)
+			restored := NewIndexedLachesis(store, prev.Input, &adapters.VectorToDagIndexer{Engine: vecengine.NewIndex(prev.crit, vecengine.LiteConfig(), vecengine.GetEngineCallbacks())}, prev.crit, prev.config)
 			assertar.NoError(restored.Bootstrap(prev.callback))
 
 			lchs[RESTORED].IndexedLachesis = restored
