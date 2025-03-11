04269bf49d58407aae11c7f4f3201a3f77122fa8
Матија Купрешанин <matijakup@gmail.com>
2025-03-11 13:00:35 +0100

    Vecengine is retired (in source code).

--- vecmt/vector.go
+++ vecmt/vector.go
@@ -2,9 +2,9 @@ package vecmt
 
 import (
 	"encoding/binary"
+	"math"
 
 	"github.com/0xsoniclabs/consensus/inter/idx"
-	"github.com/0xsoniclabs/consensus/vecengine"
 )
 
 /*
@@ -12,23 +12,120 @@ import (
  */
 
 type (
+	// LowestAfterSeq is a vector of lowest events (their Seq) which do observe the source event
+	LowestAfterSeq []byte
+	// HighestBeforeSeq is a vector of highest events (their Seq + IsForkDetected) which are observed by source event
+	HighestBeforeSeq []byte
+
+	// BranchSeq encodes Seq and MinSeq into 8 bytes
+	BranchSeq struct {
+		Seq    idx.Event
+		MinSeq idx.Event
+	}
+)
+
+// NewLowestAfterSeq creates new LowestAfterSeq vector.
+func NewLowestAfterSeq(size idx.Validator) *LowestAfterSeq {
+	b := make(LowestAfterSeq, size*4)
+	return &b
+}
+
+// NewHighestBeforeSeq creates new HighestBeforeSeq vector.
+func NewHighestBeforeSeq(size idx.Validator) *HighestBeforeSeq {
+	b := make(HighestBeforeSeq, size*8)
+	return &b
+}
+
+type (
 	// HighestBeforeTime is a vector of highest events (their CreationTime) which are observed by source event
 	HighestBeforeTime []byte
 
 	HighestBefore struct {
-		VSeq  *vecengine.HighestBeforeSeq
+		VSeq  *HighestBeforeSeq
 		VTime *HighestBeforeTime
 	}
+
+	LowestAfter = LowestAfterSeq
 )
 
 // NewHighestBefore creates new HighestBefore vector.
 func NewHighestBefore(size idx.Validator) *HighestBefore {
-	vSeq := make(vecengine.HighestBeforeSeq, size*8)
+	vSeq := make(HighestBeforeSeq, size*8)
 	vTime := make(HighestBeforeTime, size*8)
 	return &HighestBefore{
 		VSeq:  &vSeq,
 		VTime: &vTime,
 	}
+}
+
+type allVecs struct {
+	after  *LowestAfter
+	before *HighestBefore
+}
+
+// Get i's position in the byte-encoded vector clock
+func (b LowestAfterSeq) Get(i idx.Validator) idx.Event {
+	for i >= b.Size() {
+		return 0
+	}
+	return idx.Event(binary.LittleEndian.Uint32(b[i*4 : (i+1)*4]))
+}
+
+// Size of the vector clock
+func (b LowestAfterSeq) Size() idx.Validator {
+	return idx.Validator(len(b)) / 4
+}
+
+// Set i's position in the byte-encoded vector clock
+func (b *LowestAfterSeq) Set(i idx.Validator, seq idx.Event) {
+	for i >= b.Size() {
+		// append zeros if exceeds size
+		*b = append(*b, []byte{0, 0, 0, 0}...)
+	}
+
+	binary.LittleEndian.PutUint32((*b)[i*4:(i+1)*4], uint32(seq))
+}
+
+// Size of the vector clock
+func (b HighestBeforeSeq) Size() int {
+	return len(b) / 8
+}
+
+// Get i's position in the byte-encoded vector clock
+func (b HighestBeforeSeq) Get(i idx.Validator) BranchSeq {
+	for int(i) >= b.Size() {
+		return BranchSeq{}
+	}
+	seq1 := binary.LittleEndian.Uint32(b[i*8 : i*8+4])
+	seq2 := binary.LittleEndian.Uint32(b[i*8+4 : i*8+8])
+
+	return BranchSeq{
+		Seq:    idx.Event(seq1),
+		MinSeq: idx.Event(seq2),
+	}
+}
+
+// Set i's position in the byte-encoded vector clock
+func (b *HighestBeforeSeq) Set(i idx.Validator, seq BranchSeq) {
+	for int(i) >= b.Size() {
+		// append zeros if exceeds size
+		*b = append(*b, []byte{0, 0, 0, 0, 0, 0, 0, 0}...)
+	}
+	binary.LittleEndian.PutUint32((*b)[i*8:i*8+4], uint32(seq.Seq))
+	binary.LittleEndian.PutUint32((*b)[i*8+4:i*8+8], uint32(seq.MinSeq))
+}
+
+var (
+	// forkDetectedSeq is a special marker of observed fork by a creator
+	forkDetectedSeq = BranchSeq{
+		Seq:    0,
+		MinSeq: idx.Event(math.MaxInt32),
+	}
+)
+
+// IsForkDetected returns true if observed fork by a creator (in combination of branches)
+func (seq BranchSeq) IsForkDetected() bool {
+	return seq == forkDetectedSeq
 }
 
 // Get i's position in the byte-encoded vector clock
--- vecmt/vector_ops.go
+++ vecmt/vector_ops.go
@@ -3,7 +3,6 @@ package vecmt
 import (
 	"github.com/0xsoniclabs/consensus/inter/dag"
 	"github.com/0xsoniclabs/consensus/inter/idx"
-	"github.com/0xsoniclabs/consensus/vecengine"
 )
 
 type CreationTimer interface {
@@ -11,32 +10,45 @@ type CreationTimer interface {
 }
 
 func (b *HighestBefore) InitWithEvent(i idx.Validator, e dag.Event) {
-	b.VSeq.InitWithEvent(i, e)
+	b.VSeq.Set(i, BranchSeq{Seq: e.Seq(), MinSeq: e.Seq()})
 	b.VTime.Set(i, e.(CreationTimer).CreationTime())
 }
 
+func (b *LowestAfter) InitWithEvent(i idx.Validator, e dag.Event) {
+	b.Set(i, e.Seq())
+}
+
+func (b *LowestAfter) Visit(i idx.Validator, e dag.Event) bool {
+	if b.Get(i) != 0 {
+		return false
+	}
+
+	b.Set(i, e.Seq())
+	return true
+}
+
 func (b *HighestBefore) IsEmpty(i idx.Validator) bool {
-	return b.VSeq.IsEmpty(i)
+	seq := b.VSeq.Get(i)
+	return !seq.IsForkDetected() && seq.Seq == 0
 }
 
 func (b *HighestBefore) IsForkDetected(i idx.Validator) bool {
-	return b.VSeq.IsForkDetected(i)
+	return b.VSeq.Get(i).IsForkDetected()
 }
 
 func (b *HighestBefore) Seq(i idx.Validator) idx.Event {
-	return b.VSeq.Seq(i)
+	return b.VSeq.Get(i).Seq
 }
 
 func (b *HighestBefore) MinSeq(i idx.Validator) idx.Event {
-	return b.VSeq.MinSeq(i)
+	return b.VSeq.Get(i).MinSeq
 }
 
 func (b *HighestBefore) SetForkDetected(i idx.Validator) {
-	b.VSeq.SetForkDetected(i)
+	b.VSeq.Set(i, forkDetectedSeq)
 }
 
-func (hb *HighestBefore) CollectFrom(_other vecengine.HighestBeforeI, num idx.Validator) {
-	other := _other.(*HighestBefore)
+func (hb *HighestBefore) CollectFrom(other *HighestBefore, num idx.Validator) {
 	for branchID := idx.Validator(0); branchID < num; branchID++ {
 		hisSeq := other.VSeq.Get(branchID)
 		if hisSeq.Seq == 0 && !hisSeq.IsForkDetected() {
@@ -68,10 +80,9 @@ func (hb *HighestBefore) CollectFrom(_other vecengine.HighestBeforeI, num idx.Va
 	}
 }
 
-func (hb *HighestBefore) GatherFrom(to idx.Validator, _other vecengine.HighestBeforeI, from []idx.Validator) {
-	other := _other.(*HighestBefore)
+func (hb *HighestBefore) GatherFrom(to idx.Validator, other *HighestBefore, from []idx.Validator) {
 	// read all branches to find highest event
-	highestBranchSeq := vecengine.BranchSeq{}
+	highestBranchSeq := BranchSeq{}
 	highestBranchTime := Timestamp(0)
 	for _, branchID := range from {
 		vseq := other.VSeq.Get(branchID)
