cff4937da15d8278030b33967a8b10df2734bca3
Filip Drobnjaković <drobnjakovicfilip@gmail.com>
2025-04-16 19:33:48 +0200

    add consensusengine unit tests

--- consensus/consensusengine/frame_decide_test.go
+++ consensus/consensusengine/frame_decide_test.go
@@ -65,7 +65,7 @@ func testConfirmBlocks(t *testing.T, weights []consensus.Weight, cheatersCount i
 	assertar := assert.New(t)
 
 	nodes := consensustest.GenNodes(len(weights))
-	lch, _, input, _ := NewCoreLachesis(nodes, weights)
+	lch, _, input, _ := NewBootstrappedCoreConesensus(nodes, weights)
 
 	var (
 		frames []consensus.Frame
