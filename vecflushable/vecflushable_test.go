7cdc939d6ceb18a5dde2006a9b185a615fd1f560
Matija Kupresanin <matijakup@gmail.com>
2025-04-16 16:54:56 +0200

    Added some tests.

--- vecflushable/vecflushable_test.go
+++ vecflushable/vecflushable_test.go
@@ -242,3 +242,77 @@ func tempLevelDB() (kvdb.Store, error) {
 	ldb, _ := disk.OpenDB("0")
 	return ldb, nil
 }
+
+func TestNilParentWrap(t *testing.T) {
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("expected panic, got nil")
+		}
+	}()
+	_ = Wrap(nil, 1000)
+}
+
+func TestNilPut(t *testing.T) {
+	backupDB, _ := tempLevelDB()
+	vecflushable := Wrap(backupDB, 1000)
+
+	err := vecflushable.Put([]byte("a"), nil)
+	if err == nil {
+		t.Error("expected error, got nil")
+	}
+
+	err = vecflushable.Put(nil, []byte("a"))
+	if err == nil {
+		t.Error("expected error, got nil")
+	}
+}
+
+func TestHasInModified(t *testing.T) {
+	backupDB, _ := tempLevelDB()
+	vecflushable := Wrap(backupDB, 1000)
+
+	key := byteutils.Uint64ToBigEndian(uint64(0))
+	val := byteutils.Uint64ToBigEndian(uint64(1))
+
+	if err := vecflushable.Put(key, val); err != nil {
+		t.Error(err)
+	}
+
+	has, err := vecflushable.Has(key)
+	if err != nil {
+		t.Error(err)
+	}
+	assert.True(t, has)
+
+	has, err = vecflushable.Has(byteutils.Uint64ToBigEndian(uint64(2)))
+	if err != nil {
+		t.Error(err)
+	}
+	assert.False(t, has)
+}
+
+func TestHasInBacked(t *testing.T) {
+	backupDB, _ := tempLevelDB()
+	vecflushable := Wrap(backupDB, 1000)
+
+	key := byteutils.Uint64ToBigEndian(uint64(0))
+	val := byteutils.Uint64ToBigEndian(uint64(1))
+
+	if err := vecflushable.Put(key, val); err != nil {
+		t.Error(err)
+	}
+	// Force contents to underlying
+	vecflushable.Flush()
+
+	has, err := vecflushable.Has(key)
+	if err != nil {
+		t.Error(err)
+	}
+	assert.True(t, has)
+
+	has, err = vecflushable.Has(byteutils.Uint64ToBigEndian(uint64(2)))
+	if err != nil {
+		t.Error(err)
+	}
+	assert.False(t, has)
+}
