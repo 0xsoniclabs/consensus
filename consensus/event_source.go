e709e534b95af0df2ae5a58011d77ae8476f8899
Bernhard Scholz <14209113+b-scholz@users.noreply.github.com>
2026-04-04 22:40:50 +1100

    Fix license header

--- consensus/event_source.go
+++ consensus/event_source.go
@@ -8,14 +8,6 @@
 // On the date above, in accordance with the Business Source License, use of
 // this software will be governed by the GNU Lesser General Public License v3.
 
-// Use of this software is governed by the Business Source License included
-// in the LICENSE file and at fantom.foundation/bsl11.
-//
-// Change Date: 2028-4-16
-//
-// On the date above, in accordance with the Business Source License, use of
-// this software will be governed by the GNU Lesser General Public License v3.
-
 package consensus
 
 // EventSource is a callback for getting events from an external storage.
